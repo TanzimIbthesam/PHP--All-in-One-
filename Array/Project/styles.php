0026bd950b99c56ac7935e36b9b0e66b05cc352c
TanzimIbthesam <35595427+TanzimIbthesam@users.noreply.github.com>
2019-05-04 00:24:23 +0600

    Add files via upload

---
+++ Array/Project/styles.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<style> 
+header{
+    background:<?php echo $site['style']['bgcolor']; ?>;
+    min-height:200px;
+    
+    padding:30px;
+    font-size:30px;
+}
+nav{
+  background:<?php echo $site['style']['bgcolor'];?>;
+  padding:10px 5px;
+  margin:5px 0px;
+  border-radius:<?php echo $site['style']['radius']; ?>px;
+}
+section{
+    background:<?php echo $site['style']['bgcolor'];?>;
+   
+  padding:10px 5px;
+  margin:5px 0px;
+  border-radius:<?php echo $site['style']['radius']; ?>px; 
+  min-height:600px;
+}
+footer{
+    background:grey;
+  padding:10px 5px;
+  margin:5px 0px;
+  border-radius:<?php echo $site['style']['radius']; ?>px;
+  text-align:center;
+}
+
+
+
+
+</style>
+<body>
+    
+</body>
+</html>
+
+
