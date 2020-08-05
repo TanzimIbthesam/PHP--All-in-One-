2fd6ae85f9a59c9247cb93a6fd98c0bf37540600
iamtanzim <tanzim67@gmail.com>
2020-08-05 17:24:09 +0600

    MYSQL functions and OOP updated

--- Basics/constructor/Employee.php
+++ Basics/constructor/Employee.php
@@ -1,34 +1,45 @@
 <?php 
-class Employee{
-public $age;
-public $designation;
-public function __construct($age,$designation)
-{
-    $this->age=$age;
-    $this->designation=$designation;
-}
-public function profile()
-{
-    echo "His age is {$this->age} and designation is {$this->designation}","<br>";
-}
-}
-class Admin extends Employee{
-    public $role;
-    public function __construct($age, $designation,$role)
-    {
-        parent:: __construct($age,$designation);
+$age=25;
+$agetwo=25.55;
+$fruits=[
+    'Apple',
+    'Mango',
+    'Fruits',
+];
+$condition=true;
+var_dump($condition);
+var_dump($fruits);
+var_dump($agetwo);
+// class Employee{
+// public $age;
+// public $designation;
+// public function __construct($age,$designation)
+// {
+//     $this->age=$age;
+//     $this->designation=$designation;
+// }
+// public function profile()
+// {
+//     echo "His age is {$this->age} and designation is {$this->designation}","<br>";
+// }
+// }
+// class Admin extends Employee{
+//     public $role;
+//     public function __construct($age, $designation,$role)
+//     {
+//         parent:: __construct($age,$designation);
 
         
-        $this->role=$role;
-    }
-    public function adminRole()
-    {
-        echo "His age is {$this->age} and designation is {$this->designation} and his role is {$this->role}", "<br>";
-    }
-}
+//         $this->role=$role;
+//     }
+//     public function adminRole()
+//     {
+//         echo "His age is {$this->age} and designation is {$this->designation} and his role is {$this->role}", "<br>";
+//     }
+// }
 
-$employee=new Employee(20,'Executive');
-$employee->profile();
-$admin=new Admin(20,"Executive","Admin");
-$admin->adminRole();
+// $employee=new Employee(20,'Executive');
+// $employee->profile();
+// $admin=new Admin(20,"Executive","Admin");
+// $admin->adminRole();
 
